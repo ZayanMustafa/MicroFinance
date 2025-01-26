f9db2f78bf5997ac219485b557ccb4c05cbb2af0
Zayyan Mustafa <mustafazayyan13@gmail.com>
2025-01-25 19:22:21 -0800

    admin nav enter

---
+++ src/pages/admin/adminmain.jsx
@@ -0,0 +1,11 @@
+import  React   from 'react';
+import Navbar from '../../components/navbar';
+
+const AdminMain = () => {   
+    return (
+        <Navbar/>
+            
+    );
+};
+
+export default AdminMain;
